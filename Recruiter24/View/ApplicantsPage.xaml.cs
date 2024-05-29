434603da2f148ef5efbc776e93786283e228434c
mohammedyasin4 <leonahmad4@gmail.com>
2024-05-29 21:02:28 +0200

    Added swipe functionality + refactor

--- Recruiter24/View/ApplicantsPage.xaml.cs
+++ Recruiter24/View/ApplicantsPage.xaml.cs
@@ -1,4 +1,5 @@
 using Recruiter24.Models;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net.Http.Json;
@@ -8,6 +9,8 @@ namespace Recruiter24.View;
 public partial class ApplicantsPage : ContentPage
 {
     public ObservableCollection<Applicant> Applicants = new();
+    private int yoe;
+    private Technology technology;
 
 	public ApplicantsPage()
 	{
@@ -17,40 +20,43 @@ public partial class ApplicantsPage : ContentPage
     public ApplicantsPage(int yearsOfExperience, Technology technology)
     {
         InitializeComponent();
-        _ = GetApplicants(technology.Guid, yearsOfExperience);
+        this.yoe = yearsOfExperience;
+        this.technology = technology;
     }
 
-    public async Task<ObservableCollection<Applicant>> GetApplicants(Guid guid, int yoe)
+    protected override async void OnAppearing()
+    {
+        await GetApplicants(this.technology.Guid, yoe);
+    }
+
+    public async Task GetApplicants(Guid guid, int yoe)
     {
         try
         {
             using (HttpClient client = new HttpClient())
             {
                 HttpResponseMessage res = await client.GetAsync("https://app.ifs.aero/EternalBlue/api/candidates");
-                Applicants = await res.Content.ReadFromJsonAsync<ObservableCollection<Applicant>>();
-                var query = Applicants.Where(a => a.Experience.Any(e => e.TechnologyId == guid) && a.Experience.Any(e => e.YearsOfExperience >= yoe));
-                collectionView.ItemsSource = query;
-                
-                return new ObservableCollection<Applicant>(query);
+                var json = await res.Content.ReadFromJsonAsync<ObservableCollection<Applicant>>();
+                var query = json.Where(a => a.Experience.Any(e => e.TechnologyId == guid) && a.Experience.Any(e => e.YearsOfExperience >= yoe));
+                Applicants = new ObservableCollection<Applicant>(query);
+                collectionView.ItemsSource = Applicants;
             }
         }
         catch (Exception e)
         {
             Debug.WriteLine("Error accorred: " + e.Message);
-            return new ObservableCollection<Applicant>();
         }
-        
-
     }
 
-    private void Delete_Invoked(object sender, EventArgs e)
+    private void Delete_Invoked(object sender, SwipeItems e)
     {
-        Debug.WriteLine("Delete swipe");
+        // Applicant a = sender as Applicant; 
+        Debug.WriteLine("Delete swipe: " + e.ToString());
     }
 
-    private void Save_Invoked(object sender, EventArgs e)
+    private void Save_Invoked(object sender, SwipeItem e)
     {
-        Debug.WriteLine("Save swipe");
+        Debug.WriteLine("Save swipe: " + e.ToString());
 
     }
 }
